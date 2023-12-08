dd0b82c9014a9e2fab76b10b337fc00bb34c2502
Ruh Adam <avuclariminicinde@gmail.com>
2023-12-08 13:31:16 +0300

    added gitignore

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const router = require('./routes/route');
 app.set('view engine', 'ejs');
 app.use('/css', express.static(path.join(__dirname, 'views', 'assets', 'css')));
 app.use('/flowbite', express.static(path.join(__dirname, 'node_modules', 'flowbite', 'dist')));
-app.use('/flowbite', express.static(path.join(__dirname, 'node_modules', '', 'dist')));
+
 app.use(express.urlencoded({ extended: false}))
 
 
