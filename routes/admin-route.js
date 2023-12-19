434d026516b9e4a9cda8ffb80f14176a74f6d31e
Ruh Adam <avuclariminicinde@gmail.com>
2023-12-19 21:43:40 +0300

    added new choice and fixed some problems

--- routes/admin-route.js
+++ routes/admin-route.js
@@ -31,14 +31,14 @@ router.get('/admin/logout', (req, res) => {
       console.error('Logout error:', err);
       res.status(500).send('Internal Server Error');
     } else {
-      
+
       res.redirect('/admin/login');
     }
   });
 });
 
-router.get('/admin/login',(req,res) =>{
-res.render('admin/login');
+router.get('/admin/login', (req, res) => {
+  res.render('admin/login');
 });
 
 
@@ -53,7 +53,7 @@ const requireAdminLogin = (req, res, next) => {
 
 
 //ana sayfa
-router.get('/admin',requireAdminLogin, async (req, res) => {
+router.get('/admin', requireAdminLogin, async (req, res) => {
   try {
     const kategoriResult = await queryAsync(`SELECT COUNT(*) AS kategori_sayisi FROM kategoriler`);
     const testResult = await queryAsync(`SELECT COUNT(*) AS test_sayisi FROM testler`);
@@ -77,7 +77,7 @@ router.get('/admin',requireAdminLogin, async (req, res) => {
 });
 
 //sorular sayfası
-router.get('/admin/sorular',requireAdminLogin, requireAdminLogin, async (req, res) => {
+router.get('/admin/sorular', requireAdminLogin, requireAdminLogin, async (req, res) => {
   try {
     const sorular = await queryAsync(`SELECT * FROM sorular`);
     const data = {
@@ -93,73 +93,52 @@ router.get('/admin/sorular',requireAdminLogin, requireAdminLogin, async (req, re
   }
 });
 
-router.get('/admin/soru-duzenle/:soru_id',requireAdminLogin, (req,res) => {
-let soru_id = req.params.soru_id;
-db.query(`SELECT * FROM sorular WHERE soru_id = ?`,[soru_id], (err,sorular)=>{
+router.get('/admin/soru-duzenle/:soru_id', requireAdminLogin, (req, res) => {
+  let soru_id = req.params.soru_id;
+  db.query(`SELECT * FROM sorular WHERE soru_id = ?`, [soru_id], (err, sorular) => {
 
-  const data = {
-    value: "admin-soru-duzenle",
-    title: "Admin-Soru-duzenle",
-    dataList: sorular
-  }
-  res.render('admin/admin-index',data)
+    const data = {
+      value: "admin-soru-duzenle",
+      title: "Admin-Soru-duzenle",
+      dataList: sorular
+    }
+    res.render('admin/admin-index', data)
+  });
 });
-});
 
-router.post('/admin/soru-duzenle/:soru_id',requireAdminLogin, (req,res) =>{
-let soru_id = req.params.soru_id;
-let test_id = req.body.test_id;
-let soru_metni = req.body.soru_metni;
-let soru_aciklama = req.body.soru_aciklama;
-let secenek_a = req.body.secenek_a;
-let secenek_b = req.body.secenek_b;
-let secenek_c = req.body.secenek_c;
-let secenek_d = req.body.secenek_d;
-let dogru_cevap = req.body.dogru_cevap;
+router.post('/admin/soru-duzenle/:soru_id', requireAdminLogin, (req, res) => {
+  let soru_id = req.params.soru_id;
+  let test_id = req.body.test_id;
+  let soru_metni = req.body.soru_metni;
+  let soru_aciklama = req.body.soru_aciklama;
+  let secenek_a = req.body.secenek_a;
+  let secenek_b = req.body.secenek_b;
+  let secenek_c = req.body.secenek_c;
+  let secenek_d = req.body.secenek_d;
+  let secenek_e = req.body.secenek_e;
+  let dogru_cevap = req.body.dogru_cevap;
 
 
-const sql = `
+  const sql = `
 UPDATE sorular
-SET test_id = ?, soru_metni = ?, soru_aciklama = ?, secenek_a = ?, secenek_b = ?, secenek_c = ?, secenek_d = ?, dogru_cevap = ?
+SET test_id = ?, soru_metni = ?, soru_aciklama = ?, secenek_a = ?, secenek_b = ?, secenek_c = ?, secenek_d = ?,secenek_e = ?, dogru_cevap = ?
 WHERE soru_id = ?;
 `;
 
-db.query(sql, [test_id,soru_metni, soru_aciklama, secenek_a, secenek_b, secenek_c, secenek_d, dogru_cevap, soru_id], (error,results) => {
-  if (error) {
-    console.error('Error updating record:', error);
-    res.status(500).send('Internal Server Error');
-} else {
-   res.redirect('/admin/sorular');
-}
-})
+  db.query(sql, [test_id, soru_metni, soru_aciklama, secenek_a, secenek_b, secenek_c, secenek_d, secenek_e, dogru_cevap, soru_id], (error, results) => {
+    if (error) {
+      console.error('Error updating record:', error);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.redirect('/admin/sorular');
+    }
+  })
 
 });
 
-//sorular güncelleme 
-router.post('/update/:soruId',requireAdminLogin, (req, res) => {
-  const soruId = req.params.soruId;
-  const soruMetni = req.body.soru_metni_edit;
-  const secenekA = req.body.secenekA_edit;
-  const secenekB = req.body.secenekB_edit;
-  const secenekC = req.body.secenekC_edit;
-  const secenekD = req.body.secenekD_edit;
-  const dogruCevap = req.body.dogru_cevap_edit;
-  const soruAciklama = req.body.soru_aciklama_edit;
-
-  const query = `
-    UPDATE sorular
-    SET soru_metni = ?, secenek_a = ?, secenek_b = ?, secenek_c = ?, secenek_d = ?, dogru_cevap = ?, soru_aciklama = ?
-    WHERE soru_id = ?
-  `;
-  db.query(query, [soruMetni, secenekA, secenekB, secenekC, secenekD, dogruCevap, soruAciklama, soruId], (error, results) => {
-    if (error) throw error;
-    console.log('Soru güncellendi:', results);
-    res.redirect('admin/sorular');
-  });
-})
 
 //test ekleme sayfası  
-router.get('/admin/test-ekle',requireAdminLogin, (req, res) => {
+router.get('/admin/test-ekle', requireAdminLogin, (req, res) => {
   db.query('SELECT * FROM kategoriler', (err, sonuc) => {
     const data = {
       value: "admin-test",
@@ -175,7 +154,7 @@ router.get('/admin/test-ekle',requireAdminLogin, (req, res) => {
 });
 
 //test ekleme sayfası post
-router.post('/admin/test-ekle',requireAdminLogin, (req, res) => {
+router.post('/admin/test-ekle', requireAdminLogin, (req, res) => {
   let test_id = req.body.test;
   let soru_metni = req.body.soru_metni;
   let soru_aciklama = req.body.soru_aciklama;
@@ -183,16 +162,17 @@ router.post('/admin/test-ekle',requireAdminLogin, (req, res) => {
   let secenek_B = req.body.secenekB;
   let secenek_C = req.body.secenekC;
   let secenek_D = req.body.secenekD;
+  let secenek_E = req.body.secenekE;
   let dogru_cevap = req.body.dogru_cevap;
 
 
 
   const query = `
-    INSERT INTO sorular (test_id, soru_metni, soru_aciklama, secenek_a, secenek_b, secenek_c, secenek_d, dogru_cevap)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    INSERT INTO sorular (test_id, soru_metni, soru_aciklama, secenek_a, secenek_b, secenek_c, secenek_d,secenek_e, dogru_cevap)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
   `;
 
-  db.query(query, [test_id, soru_metni, soru_aciklama, secenek_A, secenek_B, secenek_C, secenek_D, dogru_cevap], (err, result) => {
+  db.query(query, [test_id, soru_metni, soru_aciklama, secenek_A, secenek_B, secenek_C, secenek_D,secenek_E, dogru_cevap], (err, result) => {
     if (err) {
       console.error('Soru eklenirken hata oluştu:', err);
       // Hata durumunda bir şey yapabilirsiniz, örneğin istemciye bir hata mesajı gönderebilirsiniz.
@@ -205,7 +185,7 @@ router.post('/admin/test-ekle',requireAdminLogin, (req, res) => {
 });
 
 
-router.get('/admin/edit-question/:soru_id',requireAdminLogin, (req, res) => {
+router.get('/admin/edit-question/:soru_id', requireAdminLogin, (req, res) => {
   let soru_id = req.params.soru_id;
   console.log(soru_id);
   db.query(`SELECT * FROM sorular WHERE soru_id = ?`, [soru_id], (err, result) => {
@@ -214,7 +194,7 @@ router.get('/admin/edit-question/:soru_id',requireAdminLogin, (req, res) => {
       // Hatasını uygun bir şekilde işleyin, örneğin istemciye bir hata yanıtı gönderin.
       res.status(500).json({ hata: 'Soru alınırken hata oluştu' });
     } else {
-     
+
       res.status(200).json(result[0]); // result bir dizi olduğundan, [0] ile ilk öğeyi alıyoruz.
     }
   });
@@ -222,7 +202,7 @@ router.get('/admin/edit-question/:soru_id',requireAdminLogin, (req, res) => {
 })
 
 //kategori ekleme sayfası
-router.get('/admin/kategori-ekle',requireAdminLogin, (req, res) => {
+router.get('/admin/kategori-ekle', requireAdminLogin, (req, res) => {
   db.query('SELECT * FROM kategoriler ORDER BY kategori_id DESC;', (err, sonuc) => {
     const data = {
       value: "admin-kategori",
@@ -239,7 +219,7 @@ router.get('/admin/kategori-ekle',requireAdminLogin, (req, res) => {
 
 
 //kategori seçme
-router.get('/admin/create-category/:categoryId',requireAdminLogin, (req, res) => {
+router.get('/admin/create-category/:categoryId', requireAdminLogin, (req, res) => {
   const categoryId = req.params.categoryId;
 
   // Kategoriye ait testleri çek
@@ -254,7 +234,7 @@ router.get('/admin/create-category/:categoryId',requireAdminLogin, (req, res) =>
 });
 
 //kategori ekleme post
-router.post('/admin/kategori-ekle',requireAdminLogin, (req, res) => {
+router.post('/admin/kategori-ekle', requireAdminLogin, (req, res) => {
 
   let baslik = req.body.baslik;
   let aciklama = req.body.aciklama;
@@ -266,10 +246,10 @@ router.post('/admin/kategori-ekle',requireAdminLogin, (req, res) => {
 });
 
 //Kategoriye ait testleri çek
-router.get('/admin/get-tests/:categoryId',requireAdminLogin, (req, res) => {
+router.get('/admin/get-tests/:categoryId', requireAdminLogin, (req, res) => {
   const categoryId = req.params.categoryId;
 
- 
+
   db.query('SELECT * FROM testler WHERE kategori_id = ?', [categoryId], (err, tests) => {
     if (err) {
       console.error(err);
@@ -280,15 +260,15 @@ router.get('/admin/get-tests/:categoryId',requireAdminLogin, (req, res) => {
   });
 });
 //testleri listeler
-router.get('/admin/get-tests',requireAdminLogin, (req,res) => {
-  db.query(`SELECT * FROM testler `, (err,results)=>{
+router.get('/admin/get-tests', requireAdminLogin, (req, res) => {
+  db.query(`SELECT * FROM testler `, (err, results) => {
     res.send(results);
 
   })
 })
 
 //Soru Ekleme sayfası
-router.get('/admin/create/:kategori_id/:kategori_ad/:kategori_aciklama/:test_ad',requireAdminLogin, (req, res) => {
+router.get('/admin/create/:kategori_id/:kategori_ad/:kategori_aciklama/:test_ad', requireAdminLogin, (req, res) => {
   const kategori_ad = req.params.kategori_ad;
   const kategori_id = req.params.kategori_id;
   const kategori_aciklama = req.params.kategori_aciklama;
@@ -307,7 +287,7 @@ router.get('/admin/create/:kategori_id/:kategori_ad/:kategori_aciklama/:test_ad'
 });
 
 //testid ye ait soruları listeler
-router.get('/admin/get-test/:test_id',requireAdminLogin, (req, res) => {
+router.get('/admin/get-test/:test_id', requireAdminLogin, (req, res) => {
   let test_id = req.params.test_id;
 
 
@@ -323,7 +303,7 @@ router.get('/admin/get-test/:test_id',requireAdminLogin, (req, res) => {
 
 });
 //soru silme sayfası
-router.delete('/admin/delete-question/:soru_id',requireAdminLogin, (req, res) => {
+router.delete('/admin/delete-question/:soru_id', requireAdminLogin, (req, res) => {
   const soru_id = req.params.soru_id;
 
   db.query('DELETE FROM sorular WHERE soru_id = ?', [soru_id], (err, result) => {
@@ -336,7 +316,7 @@ router.delete('/admin/delete-question/:soru_id',requireAdminLogin, (req, res) =>
   });
 });
 //kategori silme sayfası
-router.delete('/admin/delete-kategori/:kategori_id',requireAdminLogin, (req, res) => {
+router.delete('/admin/delete-kategori/:kategori_id', requireAdminLogin, (req, res) => {
   const kategori_id = req.params.kategori_id;
 
   db.query('DELETE FROM kategoriler WHERE kategori_id = ?', [kategori_id], (err, result) => {
